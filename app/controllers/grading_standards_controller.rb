7a0d47f890c037d158c536afca975dc53f38ce7a
keanceva <keanceva@espol.edu.ec>
2021-01-06 01:16:47 -0500

    Funciones del controlador y rutas.

--- app/controllers/grading_standards_controller.rb
+++ app/controllers/grading_standards_controller.rb
@@ -4,8 +4,30 @@ class GradingStandardsController < ApplicationController
   end
 
   def new
+    @grading_standards = GradingStandard.new
   end
 
   def edit
+    @grading_standards = GradingStandard.find(params[:id])
+  end
+
+  def update
+    @grading_standards = GradingStandard.find(params[:id])
+    if @grading_standards.update(grading_params)
+      redirect_to grading_standars_path
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @grading_standards = GradingStandard.find(params[:id])
+    @grading_standards.destroy
+    redirect_to grading_standars_path
+  end
+
+  private
+  def grading_params
+    params.require(:grading_standards).permit(:title, :data, :usage_count)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'grading_standards/index'
   get 'grading_standards/new'
   get 'grading_standards/edit'
+  delete 'grading_standards/:id'
   resources :grading_standards
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
